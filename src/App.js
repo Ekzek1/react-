a5f2f1c66ac3ecefc8d6aeb7c9665fe533d07d6a
mishaDrigval <drigval.ma.11@gmail.com>
2022-11-02 16:52:26 +0300

    react first dev

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef} from "react";
+// import Counter from "./components/Counter";
+// import ClassCounter from  './components/ClassCounter'
+import './styles/App.css'
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Javascript 2', body: 'Description'},
+    {id: 3, title: 'Javascript 3', body: 'Description'}
+  ])
+
+  const createPost = (newPost) =>{
+    setPosts([...posts, newPost])
+  }
+
+  // Получаем post из дочернего компонента 
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostForm create={createPost} />
+      <PostList remove = {removePost} posts={posts} title="Посты про js" />
     </div>
   );
 }
